70457723193736825f21db9d960d0f0e25e412a9
Suppoze <tegyukfel@gmail.com>
2017-12-11 18:10:48 +0100

    Populating project files

---
+++ src/main/java/hu/zsoki/cinegrow/CinegrowApplication.java
@@ -0,0 +1,12 @@
+package hu.zsoki.cinegrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinegrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinegrowApplication.class, args);
+	}
+}
