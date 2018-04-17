fff38b945ac688e587eaffc5f617e7c90a205585
Suppoze <tegyukfel@gmail.com>
2018-04-17 21:21:29 +0200

    Basic authentication? (WIP)

--- src/main/java/hu/zsoki/cinegrow/common/SpringSecurityConfig.java
+++ src/main/java/hu/zsoki/cinegrow/common/SpringSecurityConfig.java
@@ -1,7 +1,9 @@
 package hu.zsoki.cinegrow.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,30 +12,44 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
+    private static final String CLIENT_ROLE = "Client";
 
-    // roles admin allow to access /admin/**
-    // roles user allow to access /user/**
-    // custom 403 access denied handler
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final AccessDeniedHandler accessDeniedHandler;
+
+    @NonNull
+    @Value("username") // Environment variable
+    private String clientUsername;
+
+    @NonNull
+    @Value("password") // Environment variable
+    private String clientPassword;
+
+    @Autowired
+    public SpringSecurityConfig(RestAuthenticationEntryPoint restAuthenticationEntryPoint,
+                                AccessDeniedHandler accessDeniedHandler) {
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+        this.accessDeniedHandler = accessDeniedHandler;
+    }
+
+    // TODO: https://stackoverflow.com/questions/48446708/securing-spring-boot-api-with-api-key-and-secret?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable() // TODO: DON'T DISABLE
-                .authorizeRequests()
-                .antMatchers("/", "/view", "/search").permitAll()
-                .antMatchers("/swagger/**").hasAnyRole("ADMIN")
-                .anyRequest().authenticated()
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
                 .and()
-                .logout()
-                .permitAll()
+                .authorizeRequests()
+                .antMatchers("/", "/view", "/search").hasRole(CLIENT_ROLE)
                 .and()
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
     }
 
-    // create two users, admin and user
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser(clientUsername).password(clientPassword).roles(CLIENT_ROLE);
     }
 }
